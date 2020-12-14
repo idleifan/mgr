6ff2a83c7228380d418f797c8cf83b637883fb92
leifan2000 <1324240314@qq.com>
2020-12-14 20:38:50 +0800

    完成基础布局

---
+++ mgr-fe/src/layout/BasicLayout/index.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="basic-layout">
+    <div class="app-header">
+        <div class="left">
+            <img src="../../assets/logo.png">
+            <div class="title">校园失物招领系统</div>
+        </div>
+        <div class="right">
+            <div class="hello-msg">你好，xx</div>
+            <div class="logout">退出</div>
+        </div>
+    </div>
+    <div class="app-header-placeholder"></div>
+    <div class="app-content">
+        <div class="left">
+            <app-nav />
+        </div>
+        <div class="right">
+            <router-view />
+        </div>
+
+    </div>
+    </div>
+</template>
+
+    <script src="./index.js"></script>
+    <style lang="scss" scoped>
+       @import './index.scss';
+    </style>
--- mgr-fe/src/router/index.js
+++ mgr-fe/src/router/index.js
@@ -7,14 +7,24 @@ const routes = [
     name: 'Auth',
     component:  () => import(/* webpackChunkName: "auth" */ '../views/Auth/index.vue'),
   },
-  //{
-  //  path: '/about',
-    //name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    //component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  //},
+  {
+    path: '/',
+    name: 'BasicLayout',
+    component:  () => import(/* webpackChunkName: "BasicLayout" */ '../layout/BasicLayout/index.vue'),
+    children: [
+      {
+        path: 'bos',
+        name: 'Bos',
+        component:  () => import(/* webpackChunkName: "Bos" */ '../views/Bos/index.vue'),
+      },
+      // {
+      //   path: 'users',
+      //   name: 'User',
+      //   component:  () => import(/* webpackChunkName: "Bos" */ '../views/User/index.vue'),
+      // },
+    ],
+  },
+
 ];
 
 const router = createRouter({
